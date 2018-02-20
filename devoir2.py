5a74ac3651b2288b8a7a1a1ffd011ad0e1636406
JSBonneterre <36011449+JSBonneterre@users.noreply.github.com>
2018-02-19 21:20:50 -0500

    Update devoir2.py

--- devoir2.py
+++ devoir2.py
@@ -1,34 +1,51 @@
 #coding: utf-8
-
 import csv
+fichier = 'grants.csv'
+f1 = open(fichier, encoding = "utf-8")
+#j'avais des charmap error alors j'ai demandé au fichier d'encode en utf-8
+document = csv.reader(f1)
 
-fichier = "grants.csv"
+#on créé nos variables en fonction des 3 programmes du Fonds
 
-f1 =open(fichier,encoding = "utf-8")
-
-lignes = csv.reader(f1)
-
-next(lignes)
-
-liste = []
-
-
-for subvention in lignes:
-
-	for element in subvention:
-		word = element.split()	
-
-		#liste de plusieurs mots 
-		for words in word:
-			if words.lower() == "périodiques":
-				date = subvention[13].split("-")
-				result =(str(date[0])+": " + str(subvention))
-				liste.append(result)
-				
-final = list(set(liste))
+editeurs = "Aide aux éditeurs"
+innovation = "Innovation commerciale"
+initiative = "Initiatives collectives"
+n=0
 
 
-for x in final:
-	print(x)
-	print("")
-	print("-"*100)
+
+for ligne in document:
+
+#si le programme apparait dans les éléments des listes des subventions, on veut la date et la ligne (subvention)
+#j'affiche aussi le programme à part, pour pouvoir ajouter une autre colonne dans le nouveau fichier et les trier selon le programme
+		
+
+	if any(editeurs in element for subvention in ligne):
+		
+		date = ligne[13]
+		annee = date.split("-")
+		n+=1
+		print("-" * 100)
+		print(n,annee[0], ligne)
+		print(editeurs)
+	
+
+	if any(innovation in element for subvention in ligne):
+
+		date = ligne[13]
+		annee = date.split("-")
+		n+=1
+		
+		print("-" * 100)
+		print(n,annee[0], ligne)
+		print(innovation)
+
+
+	if any(initiative in element for subvention in ligne):
+		date = ligne[13]
+		annee = date.split("-")
+		n+=1
+			
+		print("-" * 100)
+		print(n,annee[0], ligne)
+		print(initiative)
